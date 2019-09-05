66c7e9af8c9bb3e73dd0ebe57777c26773d86e37
benjamhw <benjamin.99@live.no>
2019-09-05 13:09:23 +0200

    Oppgavefordeling

--- oslomet/cs/algdat/Oblig1/Oblig1.java
+++ oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -12,6 +12,7 @@ public class Oblig1 {
     private Oblig1() {
     }
 
+    //Edvard
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
         throw new NotImplementedException();
@@ -21,54 +22,62 @@ public class Oblig1 {
         throw new NotImplementedException();
     }
 
+    //Edvard
     ///// Oppgave 2 //////////////////////////////////////
     public static int antallUlikeSortert(int[] a) {
         throw new NotImplementedException();
     }
 
-
+    //Edvard
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
         throw new NotImplementedException();
     }
 
+    //Benjamin
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
         throw new NotImplementedException();
     }
 
+    //Benjamin
     ///// Oppgave 5 //////////////////////////////////////
     public static void rotasjon(char[] a) {
         throw new NotImplementedException();
     }
 
+    //Benjamin
     ///// Oppgave 6 //////////////////////////////////////
     public static void rotasjon(char[] a, int k) {
         throw new NotImplementedException();
     }
 
+    //Aslak
     ///// Oppgave 7 //////////////////////////////////////
     /// 7a)
     public static String flett(String s, String t) {
         throw new NotImplementedException();
     }
 
+    //Aslak
     /// 7b)
     public static String flett(String... s) {
         throw new NotImplementedException();
     }
 
+    //Aslak
     ///// Oppgave 8 //////////////////////////////////////
     public static int[] indekssortering(int[] a) {
         throw new NotImplementedException();
     }
 
-
+    //Jacob
     ///// Oppgave 9 //////////////////////////////////////
     public static int[] tredjeMin(int[] a) {
         throw new NotImplementedException();
     }
 
+    //Jacob
     ///// Oppgave 10 //////////////////////////////////////
     public static int bokstavNr(char bokstav) {
         throw new NotImplementedException();
